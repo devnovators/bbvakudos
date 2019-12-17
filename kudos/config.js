ac496f1e3b265ea9063fbf3eec34aa39f22fe034
elvis <elvis.nizama@bbva.com>
2019-12-17 04:48:06 -0500

    retirar imágenes

---
+++ kudos/config.js
@@ -0,0 +1,17 @@
+const config={
+    apiKey: "AIzaSyCQgB2pehO3brhVrS6xhd-DKBx0NWV6CfE",
+    authDomain: "bbva-kudos.firebaseapp.com",
+    databaseURL: "https://bbva-kudos.firebaseio.com",
+    projectId: "bbva-kudos",
+    storageBucket: "bbva-kudos.appspot.com",
+    messagingSenderId: "962944540684"
+}
+
+/*
+                    apiKey: config.apiKey,
+                    authDomain: config.authDomain,
+                    databaseURL: config.databaseURL,
+                    projectId: config.projectId,
+                    storageBucket: config.storageBucket,
+                    messagingSenderId: config.messagingSenderId
+*/
